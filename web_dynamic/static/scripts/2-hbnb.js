e589615548dceadf0f6fea87e0eb2a86c447e359
sumin3 <suminyu@yahoo.com>
2019-02-24 02:36:09 +0000

    changed: python file -- chaneg file name in render_template. html file -- fix syntax error, pass a variable instead of a string. css file -- fix the style overwrite issues

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,23 +1,35 @@
-/* JavaScript file
-Your script must be executed only when DOM is loaded
-You must use jQuery
-Listen for changes on each INPUT checkbox tag: */
+/* JavaScript file */
 
 $(document).ready(function () {
+  /*
+  function: handle changes on each INPUT checkbox tag
+          if the checkbox is checked, add data name to the list.
+          if the checkbox is unchecked, remove data name from the list.
+  */
   let list = [];
   $('INPUT').on('change', function () {
     if ($(this).is(':checked')) {
       list.push($(this).attr('data-name'));
-      $('h4').text(list);
+      $('.amenities h4').text(list);
     } else {
       list.splice(list.indexOf($(this).attr('data-name')), 1);
-      $('h4').text(list);
+      $('.amenities h4').text(list);
+    }
+    if (list.length === 0) {
+      $('.amenities h4').html('&nbsp;');
     }
   });
-$.get('http://0.0.0.0:5001/api/v1/status/', function (res) {
-  if (res.status === 'OK') {
-    $('DIV#api_status').addClass('available');
-  } else {
-    $('DIV#api_status').removeClass('available');
-  }
+
+  /*
+  function: check the status of a request
+          if status is 'OK', add class avaiable.
+          otherwise, remove the class avaiable.
+  */
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (res) {
+    if (res.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
 });
