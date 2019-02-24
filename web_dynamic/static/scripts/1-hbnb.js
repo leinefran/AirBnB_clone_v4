b7714f8979504c296b0b877c9173e11acf716aeb
sumin3 <suminyu@yahoo.com>
2019-02-24 02:37:17 +0000

    added: add comments, use .html to handle space issues when the list is empty

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,22 @@
-/* JavaScript file
-Your script must be executed only when DOM is loaded
-You must use jQuery
-Listen for changes on each INPUT checkbox tag: */
+/* JavaScript file */
 
 $(document).ready(function () {
+  /*
+  function: handle changes on each INPUT checkbox tag
+          if the checkbox is checked, add data name to the list.
+          if the checkbox is unchecked, remove data name from the list.
+  */
   let list = [];
   $('INPUT').on('change', function () {
     if ($(this).is(':checked')) {
       list.push($(this).attr('data-name'));
-      $('h4').text(list);
+      $('.amenities h4').text(list);
     } else {
       list.splice(list.indexOf($(this).attr('data-name')), 1);
-      $('h4').text(list);
+      $('.amenities h4').text(list);
+    }
+    if (list.length === 0) {
+      $('.amenities h4').html('&nbsp;');
     }
   });
 });
